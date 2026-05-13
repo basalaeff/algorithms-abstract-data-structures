6760a206b956ad0246b37213795ce83e70bf35a7
Андрей <lbasalaeff@gmail.com>
2026-05-13 23:19:15 +0700

    feat(1.rpn.performOperation): added method for performing operation in the expression

---
+++ src/tasks/1.rpn/1.rpn.performOperation.cpp
@@ -0,0 +1,30 @@
+#include <stdexcept>
+
+#include "1.rpn.hpp"
+
+// ============================================================================
+// Выполнение арифметической операции
+// ============================================================================
+
+int performOperation(int left, int right, char operation) {
+  switch (operation) {
+    case '+':
+      return left + right;
+
+    case '-':
+      return left - right;
+
+    case '*':
+      return left * right;
+
+    case '/':
+      if (right == 0) {
+        throw std::runtime_error("Division by zero.");
+      }
+
+      return left / right;
+
+    default:
+      throw std::runtime_error("Unknown operator.");
+  }
+}
