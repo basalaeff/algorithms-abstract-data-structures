a767f683cd88fe9e1542ccdbf3caea8826f633e6
Андрей <lbasalaeff@gmail.com>
2026-05-14 16:16:01 +0700

    feat(set): add at method

---
+++ src/ads/g.Set/g3.at.cpp
@@ -0,0 +1,28 @@
+#include "g.Set.hpp"
+
+// ============================================================================
+// A3 SET_AT
+// ============================================================================
+// Проверка наличия элемента.
+//
+// true  -> найден
+// false -> не найден
+//
+// Сложность: O(1)
+// ============================================================================
+
+bool Set::at(const std::string& value) const {
+  int index = hash(value);
+
+  Node* current = table_[index];
+
+  while (current != nullptr) {
+    if (current->data_ == value) {
+      return true;
+    }
+
+    current = current->next_;
+  }
+
+  return false;
+}
