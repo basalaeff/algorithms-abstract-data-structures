d577e5de6f1e5b2f65bf9956c53371959a07fee1
Андрей <lbasalaeff@gmail.com>
2026-05-24 21:26:36 +0700

    feat(fork): add class BinarySearchTree, constructor and destructor with a method to recursively remove all nodes

---
+++ src/ads/BinarySearchTree/BinarySearchTree.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "../Node/TreeNode.hpp"
+
+template <typename T>
+class BinarySearchTree {
+ private:
+  TreeNode<T>* root_;
+
+  TreeNode<T>* insert(TreeNode<T>* node, T value);
+  void clear(TreeNode<T>* node);
+
+  void collect(TreeNode<T>* node, std::vector<T>& out) const;
+
+ public:
+  BinarySearchTree() : root_(nullptr) {};
+  ~BinarySearchTree() { clear(root_); };
+
+  void insert(T value);
+
+  void printTwoChildren() const;
+};
+#include "bst.clear.tpp"
